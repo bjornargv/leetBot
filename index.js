c17ed416d5de80b3d8bc98b8e86d7a5933885782
KHTangent <contact@khtangent.com>
2022-12-09 18:02:59 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,100 +1,97 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require("discord.js");
+const fs = require("fs");
 
 let leetMessageCount = 0;
 const client = new Discord.Client({ intents: [3276799] });
 const counts = new Map();
 
 function loadCounts() {
-    fs.readFile('counts.json', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+	fs.readFile("counts.json", (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
 
-        const savedCounts = JSON.parse(data);
-        Object.keys(savedCounts).forEach(userId => {
-            counts.set(userId, savedCounts[userId]);
-        });
-    });
+		const savedCounts = JSON.parse(data);
+		Object.keys(savedCounts).forEach((userId) => {
+			counts.set(userId, savedCounts[userId]);
+		});
+	});
 }
 
 function saveCounts() {
+	const countsObj = {};
 
-    const countsObj = {};
+	counts.forEach((value, key) => {
+		countsObj[key] = value;
+	});
 
-    counts.forEach((value, key) => {
-
-        countsObj[key] = value;
-
-    });
-
-    fs.writeFile('counts.json', JSON.stringify(countsObj), err => {
-
-        if (err) {
-
-            console.error(err);
-
-        }
-
-    });
+	fs.writeFile("counts.json", JSON.stringify(countsObj), (err) => {
+		if (err) {
+			console.error(err);
+		}
+	});
 }
 
-client.on('ready', () => {
+client.on("ready", () => {
+	console.log("Bot is ready!");
 
-    console.log('Bot is ready!');
-
-    loadCounts();
+	loadCounts();
 });
 
-client.on('messageCreate', message => {
-    if (message.author.bot) return;
+client.on("messageCreate", (message) => {
+	if (message.author.bot) return;
 
-    const now = new Date();
-    const messageText = message.content.toLowerCase();
-    console.log(message.content);
+	const now = new Date();
+	console.log(message.content);
 
-    if (now.getHours() === 12 && now.getMinutes() === 37 && message.content.toLowerCase().includes ("leet")) {
+	if (
+		now.getHours() === 12 &&
+		now.getMinutes() === 37 &&
+		message.content.toLowerCase().includes("leet")
+	) {
+		const user = message.author;
+		const today = new Date().toDateString();
 
+		if (counts.has(user.id) && counts.get(user.id).date === today) {
+			message.react("❌");
+			return;
+		}
 
-        const user = message.author;
-        const today = new Date().toDateString();
+		if (!counts.has(user.id)) {
+			counts.set(user.id, { count: 0, date: today });
+		} else {
+			counts.get(user.id).date = today;
+		}
+		counts.get(user.id).count++;
+		message.react("✅"); // Emoji for 13:37
 
-        if (counts.has(user.id) && counts.get(user.id).date === today) {
-            message.react(`❌`)
-            return;
-        }
+		saveCounts();
+		leetMessageCount++;
 
-        if (!counts.has(user.id)) {
-            counts.set(user.id, { count: 0, date: today });
-        } else {
-            counts.get(user.id).date = today;
-        }
-        counts.get(user.id).count++;
-        message.react('✅'); // Emoji for 13:37
+		if (leetMessageCount === 1) {
+			setTimeout(() => {
+				const entries = Array.from(counts.entries()).map(
+					([userId, { count }]) => ({ userId, count })
+				);
+				entries.sort((a, b) => b.count - a.count);
 
-        saveCounts();
-        leetMessageCount++;
+				// Create a string containing the leaderboard message
+				let leaderboardMessage = "**Leaderboard:**\n";
+				for (let i = 0; i < entries.length; i++) {
+					const user = client.users.cache.get(entries[i].userId);
+					leaderboardMessage += `${i + 1}. ${user.username} - ${
+						entries[i].count
+					} leet(s)\n`;
+				}
 
-        if (leetMessageCount === 1) {
-            setTimeout(() => {
-                const entries = Array.from(counts.entries()).map(([userId, { count }]) => ({ userId, count }));
-                entries.sort((a, b) => b.count - a.count);
-
-                // Create a string containing the leaderboard message
-                let leaderboardMessage = '**Leaderboard:**\n';
-                for (let i = 0; i < entries.length; i++) {
-                    const user = client.users.cache.get(entries[i].userId);
-                    leaderboardMessage += `${i + 1}. ${user.username} - ${entries[i].count} leet(s)\n`;
-                }
-
-                message.channel.send(leaderboardMessage);
-                leetMessageCount = 0;
-            }, 60 * 1000);
-        }
-    } else if (message.content.toLowerCase().includes ("leet")) {
-        message.react('870236655624290304');
-    }
+				message.channel.send(leaderboardMessage);
+				leetMessageCount = 0;
+			}, 60 * 1000);
+		}
+	} else if (message.content.toLowerCase().includes("leet")) {
+		message.react("870236655624290304");
+	}
 });
 
 client.login("token");
